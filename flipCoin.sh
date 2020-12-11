e0adf6aaef5e87f0781e8c0cc7691738f1d8d398
Shardul Patil <patilshardulsatish@yahoo.com>
2020-12-11 08:39:01 +0530

    Refactor : added a code to display flipcoin result

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash -x
 
 echo "Well To Flip Coin Simulator"
+
+read -p "Do you want to flip a coin (y/n) : " permission
+
+if [ $permission == "y" ]
+then
+	flipResult=$(( RANDOM%2 ))
+	if [ $flipResult -eq 0 ]
+	then
+		echo "Heads"
+	else
+		echo "Tails"
+	fi
+fi
